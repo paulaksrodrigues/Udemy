215842c7aaafe3144a20d59b7cee8e227dd605c9
paulaksrodrigues <paulaksrodrigues@gmail.com>
2023-10-19 14:54:16 -0300

    atualização

--- repetições.py
+++ repetições.py
@@ -22,11 +22,41 @@ Executa uma ação enquanto uma condição for verdadeira
 # print('Acabou')
 
 
-contador = 0
+# contador = 0
 
-while contador < 10:
-    contador = contador + 1
-    print(contador)
+# while contador <= 100:
+#     contador += 1
+
+#     if contador == 6:
+#         print('Não vou mostrar o 6')
+#         continue
+    
+#     if contador >= 10 and contador <= 27:
+#         print('Não vou mostrar o', contador)
+#         continue
+    
+#     print(contador)
+
+#     if contador == 30:
+#         break
+
+# print('Acabou')
+
+# WHILE + WHILE (LAÇOS INTERNOS)
+
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+while linha <= qtd_linhas:
+    coluna = 1
+    while coluna <= qtd_colunas:
+        print(f'{linha=}, {coluna=}')
+        coluna += 1
+
+        linha += 1
 
 print('Acabou')
+
+
 
